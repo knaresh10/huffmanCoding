8d1bb091a6f70947111e14f13bc8f2f0d44e0732
Naresh <82667769+knaresh10@users.noreply.github.com>
2023-12-06 20:34:46 +0530

    Update huffmanCodingEncryption.cpp

--- huffmanCodingEncryption.cpp
+++ huffmanCodingEncryption.cpp
@@ -29,11 +29,9 @@ unordered_map<char, int> getFreqTable() {
 	ifstream inputFile("input.txt");
 	char data;
 	unordered_map<char, int> freqTable;
-
 	while (inputFile.get(data)) {
 		freqTable[data]++;
 	}
-
 	inputFile.close();
 	return freqTable;
 }
@@ -50,7 +48,6 @@ HuffmanNode *createHuffmanTree(const unordered_map<char, int> &freqTable) {
 		HuffmanNode *newNode = new HuffmanNode(it.first, it.second);
 		pq.push(newNode);
 	}
-
 	while (pq.size() != 1) {
 		HuffmanNode *first = pq.top(); pq.pop();
 		HuffmanNode *second = pq.top(); pq.pop();
@@ -72,38 +69,28 @@ void generateHuffmanCodes(const HuffmanNode *root, const string& code, unordered
 string getEncodedData(unordered_map<char, string>& huffmanCodes) {
 	string encodedData = "";
 	ifstream inputFile("input.txt");
-
 	char data;
 	while (inputFile.get(data)) encodedData += huffmanCodes[data];
-
 	inputFile.close();
 	return encodedData;
 }
 
 void writeCompressedData(const string& encodedData, const unordered_map<char, string> &huffmanCodes) {
 	ofstream outputFile("output.bin", ios::binary);
-
 	outputFile.write("HUFFMAN", 7);
-
 	char numSym = static_cast<char>(huffmanCodes.size());
 	outputFile.write(&numSym, 1);
-
-
 	for (const auto& pair : huffmanCodes) {
 		char symbol = pair.first;
 		char codeLength = static_cast<char>(pair.second.length());
-
 		outputFile.write(&symbol, 1);
 		outputFile.write(&codeLength, 1);
-
 		outputFile.write(pair.second.c_str(), codeLength);
 	}
 
 	outputFile.write("DATA", 4);
-
 	size_t originalLength = encodedData.length();
 	outputFile.write(reinterpret_cast<const char*>(&originalLength), sizeof(size_t));
-
 	for (int i = 0; i < encodedData.length(); i += 8) {
 		string byteStr = encodedData.substr(i, 8);
 		while (byteStr.length() < 8) {
@@ -112,19 +99,16 @@ void writeCompressedData(const string& encodedData, const unordered_map<char, st
 		char byte = static_cast<char>(bitset<8>(byteStr).to_ulong());
 		outputFile.put(byte);
 	}
-
 	outputFile.close();
+	cout << "encoded data is stored in  output.bin file\n";
 }
 
 int main() {
-
 	unordered_map<char, int> freqTable = getFreqTable();
 	HuffmanNode *huffmanTree = createHuffmanTree(freqTable);
-
 	unordered_map<char, string> huffmanCodes;
 	generateHuffmanCodes(huffmanTree, "", huffmanCodes);
-
 	string encodedData = getEncodedData(huffmanCodes);
-
 	writeCompressedData(encodedData, huffmanCodes);
+	cout << "encryption is done";
 }
